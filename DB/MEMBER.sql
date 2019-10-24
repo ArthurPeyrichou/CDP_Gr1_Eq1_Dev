5acd94144e6fc0286050adf6180e14f410a12894
Arthur Peyrichou <arthurpeyrichou@yahoo.fr>
2019-10-24 21:54:33 +0200

    Ajout des fichiers de création des tables MEMBER,PROJECT,INVITATION et table de joiture MEMBER_PROJECT

---
+++ DB/MEMBER.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 
+-- Généré le :  jeu. 24 oct. 2019 à 19:54
+-- Version du serveur :  5.6.43-log
+-- Version de PHP :  7.0.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `MEMBER`
+--
+
+CREATE TABLE `MEMBER` (
+  `ID` int(11) NOT NULL,
+  `PSEUDO` varchar(50) NOT NULL,
+  `MAIL` varchar(50) NOT NULL,
+  `PASSWORD` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `MEMBER`
+--
+ALTER TABLE `MEMBER`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `PSEUDO` (`PSEUDO`),
+  ADD UNIQUE KEY `MAIL` (`MAIL`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `MEMBER`
+--
+ALTER TABLE `MEMBER`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
