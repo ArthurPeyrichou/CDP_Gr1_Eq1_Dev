772eda4b5dce8af4eb0d9acc4cbc4f7dfe03d613
Sylvain Delrée <sylvain.delree@zen-dws.com>
2019-11-05 20:31:45 +0100

    Implémentation du RegisterController, en attente du formulaire pour pouvoir le terminer

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,17 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\Member;
+use App\Service\Registration\EmailAddressInUseException;
+use App\Service\Registration\MemberNameInUseException;
+use App\Service\Registration\RegistrationService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController {
-    
+
+    //FIXME Supprimer les deux premières routes quand le formulaire sera prêt.
     /**
      * @Route("/register", name = "registerGet", methods = {"GET"})
      */
     public function viewRegister() {
-    	return $this->render('member/register.html.twig');
+        return $this->render('member/register.html.twig');
     }
 
     /**
@@ -25,8 +30,34 @@ class RegistrationController extends AbstractController {
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($member);
         $entityManager->flush();
-        
+
         return $this->render('member/login.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(RegistrationService $registrationService) : Response
+    {
+        $error = '';
+        //TODO Récupérer les infos depuis le formulaire quand il sera prêt
+        if (false) {
+            $name = '';
+            $emailAddress = '';
+            $password = '';
+            try {
+                $registrationService->registerUser($name, $emailAddress, $password);
+                return $this->redirectToRoute('loginPost');
+            }
+            catch (MemberNameInUseException $e) {
+                $error = 'Le nom d\'utilisateur choisi existe déjà';
+            }
+            catch (EmailAddressInUseException $e) {
+                $error = 'L\'adresse email choisie est déjà utilisée';
+            }
+        }
+
+        return $this->render('member/register.html.twig');
     }
 
 }
