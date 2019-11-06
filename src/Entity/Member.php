174ef4061331549dffc7f11940d42ab64f7cdb19
khadija <slamakadija@hotmail.com>
2019-11-06 22:38:53 +0100

    ajout des champs contributedProjects et ownedProjects à l'entity Member

--- src/Entity/Member.php
+++ src/Entity/Member.php
@@ -27,16 +27,24 @@ class Member implements UserInterface
      * @ORM\Column(type="string")
      */
     private $password;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\PROJECT", mappedBy="owner")
+     */
+    private $ownedProjects;
+
     /**
      * @ORM\ManyToMany(targetEntity="App\Entity\PROJECT", inversedBy="members")
      */
-    private $projects;
+    private $contributedProjects;
+
     public function __construct($name, $emailAddress, $password)
     {
         $this->name = $name;
         $this->emailAddress = $emailAddress;
         $this->password = $password;
-        $this->projects = new ArrayCollection();
+        $this->ownedProjects = new ArrayCollection();
+        $this->contributedProjects = new ArrayCollection();
     }
     public function getId(): ?int
     {
@@ -84,25 +92,62 @@ class Member implements UserInterface
         $this->password = $password;
         return $this;
     }
+
     /**
      * @return Collection|PROJECT[]
      */
-    public function getProjects(): Collection
+    public function getOwnedProjects(): Collection
     {
-        return $this->projects;
+        return $this->ownedProjects;
     }
-    public function addProject(PROJECT $project): self
+
+    public function addOwnedProject(PROJECT $ownedProject): self
     {
-        if (!$this->projects->contains($project)) {
-            $this->projects[] = $project;
+        if (!$this->ownedProjects->contains($ownedProject)) {
+            $this->ownedProjects[] = $ownedProject;
+            $ownedProject->setOwner($this);
         }
+
         return $this;
     }
-    public function removeProject(PROJECT $project): self
+
+    public function removeOwnedProject(PROJECT $ownedProject): self
     {
-        if ($this->projects->contains($project)) {
-            $this->projects->removeElement($project);
+        if ($this->ownedProjects->contains($ownedProject)) {
+            $this->ownedProjects->removeElement($ownedProject);
+            // set the owning side to null (unless already changed)
+            if ($ownedProject->getOwner() === $this) {
+                $ownedProject->setOwner(null);
+            }
         }
+
         return $this;
     }
+
+    /**
+     * @return Collection|PROJECT[]
+     */
+    public function getContributedProjects(): Collection
+    {
+        return $this->contributedProjects;
+    }
+
+    public function addContributedProject(PROJECT $contributedProject): self
+    {
+        if (!$this->contributedProjects->contains($contributedProject)) {
+            $this->contributedProjects[] = $contributedProject;
+        }
+
+        return $this;
+    }
+
+    public function removeContributedProject(PROJECT $contributedProject): self
+    {
+        if ($this->contributedProjects->contains($contributedProject)) {
+            $this->contributedProjects->removeElement($contributedProject);
+        }
+
+        return $this;
+    }
+
 }
