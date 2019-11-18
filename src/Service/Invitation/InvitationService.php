47bb8c50a1cbd115546ea2ce833e5920ba53b8f9
Sylvain Delrée <sylvain.delree@zen-dws.com>
2019-11-18 17:43:09 +0100

    Suppression du code d'envoi de mail devenu inutile dans InvitationService

--- src/Service/Invitation/InvitationService.php
+++ src/Service/Invitation/InvitationService.php
@@ -53,13 +53,6 @@ class InvitationService
         $this->entityManager->persist($invitation);
         $this->entityManager->flush();
 
-        $message = '<html><body><div style="margin-left: 0; margin-right: 0; text-align: center;"><h3>Vous avez été invité à rejoindre le projet "' . $project->getName() . '"</h3></div></body></html>';
-        $headers = "From: FireScrum\r\n";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=iso-8859-1\r\n";
-        $subject = 'Invitation';
-        //mail($newMember->getUsername(),$subject,utf8_decode($message),$headers);
-
         return $invitation;
     }
 
