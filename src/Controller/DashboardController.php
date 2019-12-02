133020b6f473a2655739b1f38bc861aef4f4197d
Arthur Peyrichou <arthurpeyrichou@yahoo.fr>
2019-12-02 13:24:47 +0100

    [Bugfix] oublie import de NotificationService pour le DashBoardController

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\InvitationRepository;
+use App\Service\NotificationService;
 use App\Repository\PlanningPokerRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
