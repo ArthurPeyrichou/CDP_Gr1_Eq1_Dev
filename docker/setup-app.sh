e034368a52870226b0da1770e0e2607de1d0e4d0
Sylvain Delrée <sylvain.delree@zen-dws.com>
2019-12-10 16:08:58 +0100

    [Bugfix] Réparation du docker

--- docker/setup-app.sh
+++ docker/setup-app.sh
@@ -1,6 +1,6 @@
 DB_HOST=$(echo $DATABASE_URL | cut -d '@' -f 2 | cut -d '/' -f 1)
 
-wait-for-it DB_HOST -t 0
+wait-for-it $DB_HOST -t 0
 php bin/console doctrine:database:create
 php bin/console doctrine:migration:migrate
 php bin/console cache:warmup
